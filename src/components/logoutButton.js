eb6c63bc7545b5b9962707652203fdef191fc049
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2021-03-16 11:16:09 -0700

    Adeed Auth & Logout Button

---
+++ src/components/logoutButton.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+// import { } from ;
+
+const LogOutButton = () => {
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const [isLogged, setIsLogged] = useState(false);
+}
+// class Layout extends Component {
+//     state = {
+//         mobileOpen: false,
+//         isLogged: false,
+//     };
+
+export default LogOutButton;
+// https://medium.com/develop-and-deploy-a-complex-serverless-web-app/create-a-logout-button-listening-to-authentication-updates-within-your-aws-amplify-react-82685537bb4a
